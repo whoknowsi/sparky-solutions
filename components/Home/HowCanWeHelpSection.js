572166ca9109ba39060f3a7f46c264ba0e960677
whoknowsi <echosmania@gmail.com>
2022-12-29 20:46:47 -0300

    trying to publish on gh pages

--- components/Home/HowCanWeHelpSection.js
+++ components/Home/HowCanWeHelpSection.js
@@ -11,7 +11,7 @@ const HowCanWeHelpSection = () => {
                         How can <span className='underlinedWord'>we help</span> you?
                     </h2>
                     <p>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s
                         standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
                         a type specimen book.</p>
                     <p>
--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: 'build',
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+    }
+  },
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
 }
 
 module.exports = nextConfig
